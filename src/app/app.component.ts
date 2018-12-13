053f484f038ae0d6312e3c08ac4239d0b7abf964
vrgbrg <vrgbrg@gmail.com>
2018-12-13 10:31:08 +0100

    CAtegory enum added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { AppModel } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task-manager';
+
+  public appModel = new AppModel();
+
+  constructor() {
+  
+  }
+
+  public showAppModelValue() {
+    console.log(this.appModel);
+  }
+
+  changeChecked(isChecked: Event) {
+    this.appModel.checked = !!isChecked;
+
+    /*
+    if(isChecked) {
+      this.appModel.checked = true;
+    } else {
+      this.appModel.checked = false;
+    }
+    */
+
+  }
 }
